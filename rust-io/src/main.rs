6171bbc75a1aefbec320429db03706e91f7548ea
Tao Tao9 Luo <luotao9@lenovo.com>
2022-09-09 00:07:18 +0800

    fs operations

--- rust-io/src/main.rs
+++ rust-io/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs,
-    path::{self, Path},
+    path::{self, Path, PathBuf},
 };
 
 use chrono::{DateTime, Utc};
@@ -8,11 +8,14 @@ use chrono::{DateTime, Utc};
 fn main() {
     let path = path::PathBuf::from("simplefs.txt");
 
+    // 创建文件
     // 写入文件
     fs::write(&path, "this is content").unwrap();
     // 读取文件
     let content = fs::read(&path).unwrap();
     println!("read file: {:?}", String::from_utf8(content));
+    // 删除文件
+    fs::remove_file(path).unwrap();
 
     // 读取目录
     let dir = fs::read_dir("./").unwrap();
@@ -32,6 +35,25 @@ fn main() {
             create_time.to_string()
         );
     }
+
+    let dirpath = PathBuf::from("dir1/dir2/dir3/dir4");
+    // 创建目录
+    fs::create_dir_all(&dirpath).unwrap();
+    assert!(Path::is_dir(&dirpath));
+
+    // 删除目录及子目录
+    fs::remove_dir_all("dir1/dir2").unwrap();
+    // 删除一个非空目录
+    fs::remove_dir("dir1").unwrap();
+
+
+    //Path & PathBuf
+
+    //File 
+   // fs::File
+
+    //DirBuilder
+    //fs::DirBuilder::
 
     println!("Hello, world!");
 }
