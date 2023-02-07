ea67e4b89f89a86d718c0380fde956d85313adca
Tao Tao9 Luo <luotao9@lenovo.com>
2023-02-07 17:09:00 +0800

    打印调用堆栈

---
+++ error-learn/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+
+use anyhow::Result;
+
+fn main() {
+    println!("Hello, world!:{:?}",env::var("RUST_BACKTRACE"));
+
+    env::set_var("RUST_BACKTRACE", "full");
+    //env::set_var("RUST_BACKTRACE", "1");
+    println!("error:{:?}",env::var("RUST_BACKTRACE"));
+    //panic!("asg");
+    if let Err(e) = test_anyhow() {
+        println!("e:{:?} , backtrace:{:?}", e, e.backtrace());
+    }
+}
+
+fn test_anyhow() -> Result<i32> {
+    return Err(anyhow::anyhow!("error !!!!!!!!!"));
+
+    Ok(2)
+}
