b6dc8abb6fb6bc0658858b08f59e1a59d66ca707
Tao Tao9 Luo <luotao9@lenovo.com>
2023-03-06 00:01:04 +0800

    fltk webview

---
+++ fltk-webview-learn/src/main.rs
@@ -0,0 +1,19 @@
+use fltk::{app, prelude::*, window};
+
+fn main() {
+    let app = app::App::default();
+    let mut win = window::Window::default()
+        .with_size(800, 600)
+        .with_label("Webview");
+    let mut wv_win = window::Window::default()
+        .with_size(790, 590)
+        .center_of_parent();
+    win.end();
+    win.make_resizable(true);
+    win.show();
+
+    let mut wv = fltk_webview::Webview::create(false, &mut wv_win);
+    wv.navigate("https://baidu.com");
+    
+    app.run().unwrap();
+}
