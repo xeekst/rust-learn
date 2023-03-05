eae1b00ff89f767108c3f90ae4c6746fbcca5f90
Tao Tao9 Luo <luotao9@lenovo.com>
2023-03-06 00:05:03 +0800

    fltk app

--- gui-overview/fltk-app/src/main.rs
+++ gui-overview/fltk-app/src/main.rs
@@ -5,10 +5,12 @@ fn main() {
     let app = app::App::default();
     let mut ui = myuifile::UserInterface::make_window();
     let mut win = ui.my_win.clone();
-    ui.btn1.set_callback(move |b| {
-        b.set_label("clicked");
-        win.set_label("Button clicked");
-        println!("Works!");
-    });
+    // ui.btn1.set_callback(move |b| {
+    //     b.set_label("clicked");
+    //     win.set_label("Button clicked");
+    //     println!("Works!");
+    // });
+    win.end();
+    win.show();
     app.run().unwrap();
 }
