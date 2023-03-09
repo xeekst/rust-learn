15daeb5395d9c955b38e078f04a7142b2587e4d6
Tao Tao9 Luo <luotao9@lenovo.com>
2023-03-09 18:06:04 +0800

    macro learn

--- hello_macro/hello_macro_derive/src/lib.rs
+++ hello_macro/hello_macro_derive/src/lib.rs
@@ -5,10 +5,11 @@ use quote::quote;
 use syn;
 
 #[proc_macro_derive(HelloMacro)]
-pub fn hello_macro_derive(input:TokenStream) -> TokenStream{
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
     // Construct a representation of Rust code as a syntax tree
     // that we can manipulate
-    let ast = syn::parse(input).unwrap();
+    // rust 代码 转换为 一个语法树
+    let ast = syn::parse(input).unwrap(); // 必须 panic
 
     // Build the trait implementation
     impl_hello_macro(&ast)
@@ -16,7 +17,8 @@ pub fn hello_macro_derive(input:TokenStream) -> TokenStream{
 
 fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
-    let gen = quote!{
+    //quote! 把语法树再次转换回 rust 代码
+    let gen = quote! {
         impl HelloMacro for #name{
             fn hello_macro(){
                 println!("Hello, Macro!! my name is {}!",stringify!(#name));
--- hello_macro/src/main.rs
+++ hello_macro/src/main.rs
@@ -1,13 +1,15 @@
 use hello_macro::HelloMacro;
 
+#[derive(hello_macro_derive::HelloMacro)]
 struct Pancakes;
 
-impl HelloMacro for Pancakes {
-    fn hello_macro() {
-        println!("Hello, Macro! My name is Pancakes!");
-    }
-}
+// impl HelloMacro for Pancakes {
+//     fn hello_macro() {
+//         println!("Hello, Macro! My name is Pancakes!");
+//     }
+// }
 
 fn main() {
     Pancakes::hello_macro();
+    println!("1+1:{}", stringify!(1 < 4 > 5 & 6 + 1));
 }
