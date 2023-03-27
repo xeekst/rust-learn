d59be41dae1a3c756e5fb499e73e8881ffc8f120
Tao Tao9 Luo <luotao9@lenovo.com>
2023-03-27 10:52:36 +0800

    reqwest down file

---
+++ test-reqwest-down-file/src/main.rs
@@ -0,0 +1,26 @@
+use std::io::Cursor;
+use url::form_urlencoded::{byte_serialize, parse};
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+async fn fetch_url(url: String, file_name: String) -> Result<()> {
+    let response = reqwest::get(url).await?;
+    let mut file = std::fs::File::create(file_name)?;
+    let mut content = Cursor::new(response.bytes().await?);
+    std::io::copy(&mut content, &mut file)?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let key: String = byte_serialize(byte_serialize("HEC/HECAgentKotlin/release/22.12.12.0.00/2/HecAgent-v22.12.12.0.2-debug-signed.apk".as_bytes()).collect::<String>().as_bytes()).collect();
+    let url = format!(
+        "https://artifactory.tc.lenovo.com/ui/api/v1/download?repoKey=lenovo-release-generic&path={}&isNativeBrowsing=true",
+        key
+    );
+    println!("url:{}", url);
+    fetch_url(url.to_string(), "siriel.apk".to_string())
+        .await
+        .unwrap();
+    //https://artifactory.tc.lenovo.com/ui/api/v1/download?repoKey=lenovo-release-generic&path=HEC%252FHECAgentKotlin%252Frelease%252F22.12.12.0.00%252F2%252Fsample-app-v22.12.12.0.2-debug-signed.apk&isNativeBrowsing=true
+    //https://artifactory.tc.lenovo.com/ui/api/v1/download?repoKey=lenovo-release-generic&path=HEC%2FHECAgentKotlin%2Frelease%2F22.12.12.0.00%2F2%2FHecAgent-v22.12.12.0.2-debug-signed.apk&isNativeBrowsing=true
+}
