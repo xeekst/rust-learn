a9a584339807cca215216d827ca1239b92d0a0ad
Tao Tao9 Luo <luotao9@lenovo.com>
2023-04-14 17:36:39 +0800

    sled db operation

--- inner-db-sled-learn/src/main.rs
+++ inner-db-sled-learn/src/main.rs
@@ -1,13 +1,91 @@
 use std::{thread, time::Duration};
 
+use sled::Db;
+
+struct Data {
+    pub name: String,
+    pub value: i32,
+}
+
+fn open_db() -> Db {
+    sled::open("welcome-to-sled2.sled").expect("open")
+}
+
+fn insert_or_override() {
+    let db = open_db();
+    db.insert("ISKEY", "vvvvv").unwrap();
+}
+
+fn compare_and_swap() {
+    let db = open_db();
+    // 当ISKEY 下没值的时候才插入值
+    db.compare_and_swap("ISKEY", Option::None as Option<&str>, Some("NEWV"))
+        .unwrap();
+    // 当ISKEY 下值等于输入的值时，将替换为新值
+    db.compare_and_swap("ISKEY", Some("NEWV2"), Some("NEWV"))
+        .unwrap();
+    // 当ISKEY 下值等于输入值，新值为 None 将删除 旧值
+    db.compare_and_swap("ISKEY", Some("NEWV2"), Option::None as Option<&str>)
+        .unwrap();
+}
+
+fn scan_prefix() {
+    let db = open_db();
+    for item in db.scan_prefix("ISK") {
+        println!("item:{:?}", item);
+    }
+}
+
+fn get_gt_next() {
+    let db = open_db();
+    let kv = db.get_gt("KEY").unwrap();
+    println!("kv:{:?}", kv);
+}
+
+fn get_lt_next() {
+    let db = open_db();
+    let kv = db.get_lt("KEY").unwrap();
+    println!("kv:{:?}", kv);
+}
+
+fn get_range(){
+    let db = open_db();
+    for item in db.range("K1".."K2") {
+        println!("item:{:?}", item);
+    }
+}
+
+fn watch_event() {
+    let db = open_db();
+    let mut subscriber = db.watch_prefix(vec![]);
+    for event in subscriber.take(0) {
+        match event {
+            sled::Event::Insert{ key, value } => assert_eq!(key.as_ref(), &[0]),
+            sled::Event::Remove {key } => {}
+        }
+    }
+}
+
 fn main() {
     let tree = sled::open("welcome-to-sled2.sled").expect("open");
+    let t1 = tree.open_tree("tree1").unwrap();
+    let names = tree.tree_names();
+    for n in names {
+        println!("name:{}", std::str::from_utf8(&n).unwrap());
+    }
+    let size = tree.size_on_disk().unwrap();
+    println!("size:{}", size);
+
+    let iv = tree.get("KEY1").unwrap().unwrap();
+    let val = std::str::from_utf8(&iv).unwrap();
+    println!("get KEY1:{val}");
 
     // insert and get, similar to std's BTreeMap
     tree.insert("KEY1", "VAL1").unwrap();
+    tree.insert("KEY3", "VAL421").unwrap();
     assert_eq!(tree.get(&"KEY1"), Ok(Some(sled::IVec::from("VAL1"))));
 
-    thread::spawn(move||{
+    thread::spawn(move || {
         let tree2 = sled::open("welcome-to-sled.sled").expect("open");
         tree2.insert("KEY3", "VAL1").unwrap();
         assert_eq!(tree2.get(&"KEY1"), Ok(Some(sled::IVec::from("VAL1"))));
@@ -24,7 +102,7 @@ fn main() {
 
     // atomic compare and swap
     let r = tree
-        .compare_and_swap("KEY1", Some("VAL1"), Some("VAL2"))
+        .compare_and_swap("KEY1", Some("VAL1"), Some("VAL23"))
         .unwrap();
 
     println!("compare_and_swap:{:?}", r);
@@ -38,6 +116,26 @@ fn main() {
 
     // block until all operations are stable on disk
     // (flush_async also available to get a Future)
+
+    for i in 0..10 {
+        tree.insert(&[i], vec![i])
+            .expect("should write successfully");
+    }
+
+    let lt = tree.get_lt(&[5]).unwrap();
+    println!("lt:{:?}", lt);
+
+    let gt = tree.get_gt(&[5]).unwrap();
+    println!("gt:{:?}", gt);
+
+    for item in tree.iter() {
+        println!("iter item:{:?}", item);
+    }
+
+    for item in tree.scan_prefix("KEY") {
+        println!("scan_prefix item:{:?}", item);
+    }
+
     tree.flush().unwrap();
     println!("Hello, world!");
 }
--- test-print/src/main.rs
+++ test-print/src/main.rs
@@ -2,6 +2,83 @@ use std::{fs, path::PathBuf};
 
 use base64::{engine::general_purpose, Engine as _};
 
+use std::path::{Path};
+
+mod nfs {
+    #[derive(Clone)]
+    pub(crate) struct AuthInfo(String); // NFS session management omitted
+}
+
+mod bootp {
+    pub(crate) struct AuthInfo(); // no authentication in bootp
+}
+
+// private module, lest outside users invent their own protocol kinds!
+mod proto_trait {
+    use std::path::{Path, PathBuf};
+    use super::{bootp, nfs};
+
+    pub(crate) trait ProtoKind {
+        type AuthInfo;
+        fn auth_info(&self) -> Self::AuthInfo;
+    }
+
+    pub struct Nfs {
+        auth: nfs::AuthInfo,
+        mount_point: PathBuf,
+    }
+
+    impl Nfs {
+        pub fn mount_point(&self) -> &Path {
+            &self.mount_point
+        }
+    }
+
+    impl ProtoKind for Nfs {
+        type AuthInfo = nfs::AuthInfo;
+        fn auth_info(&self) -> Self::AuthInfo {
+            self.auth.clone()
+        }
+    }
+
+    pub struct Bootp(); // no additional metadata
+
+    impl ProtoKind for Bootp {
+        type AuthInfo = bootp::AuthInfo;
+        fn auth_info(&self) -> Self::AuthInfo {
+            bootp::AuthInfo()
+        }
+    }
+}
+
+use proto_trait::ProtoKind; // keep internal to prevent impls
+pub use proto_trait::{Nfs, Bootp}; // re-export so callers can see them
+
+struct FileDownloadRequest<P: ProtoKind> {
+    file_name: PathBuf,
+    protocol: P,
+}
+
+// all common API parts go into a generic impl block
+impl<P: ProtoKind> FileDownloadRequest<P> {
+    fn file_path(&self) -> &Path {
+        &self.file_name
+    }
+
+    fn auth_info(&self) -> P::AuthInfo {
+        self.protocol.auth_info()
+    }
+}
+
+// all protocol-specific impls go into their own block
+impl FileDownloadRequest<Nfs> {
+    fn mount_point(&self) -> &Path {
+        self.protocol.mount_point()
+    }
+}
+
+
+
 fn main() {
     let mut text = r#" 
     upstream ftp_pasv-port {    
@@ -26,23 +103,27 @@ fn main() {
         println!("{}", i);
     }
 
-    let cur_build_number_path = PathBuf::from("cur_build_number_path");
-    let mut cur_build_number = -1;
-    if cur_build_number_path.exists() {
-        cur_build_number = match fs::read_to_string(&cur_build_number_path)
-            .unwrap()
-            .parse::<i32>()
-        {
-            std::result::Result::Ok(r) => r,
-            Err(err) => -1,
-        }
-    }
-    println!("cur_build_number:{cur_build_number}");
+    // let cur_build_number_path = PathBuf::from("cur_build_number_path");
+    // let mut cur_build_number = -1;
+    // if cur_build_number_path.exists() {
+    //     cur_build_number = match fs::read_to_string(&cur_build_number_path)
+    //         .unwrap()
+    //         .parse::<i32>()
+    //     {
+    //         std::result::Result::Ok(r) => r,
+    //         Err(err) => -1,
+    //     }
+    // }
+    // println!("cur_build_number:{cur_build_number}");
 
-    let hec_package_dir = "C:/Packages/windowsAgent/plugin";
-    println!("{:?}", PathBuf::from(hec_package_dir).join("dir3"));
-    if !PathBuf::from(hec_package_dir).join("dir3").exists() {
+    // let hec_package_dir = "C:/Packages/windowsAgent/plugin";
+    // println!("{:?}", PathBuf::from(hec_package_dir).join("dir3"));
+    // if !PathBuf::from(hec_package_dir).join("dir3").exists() {
         
-        fs::create_dir_all(PathBuf::from(hec_package_dir).join("dir3")).unwrap();
-    }
+    //     fs::create_dir_all(PathBuf::from(hec_package_dir).join("dir3")).unwrap();
+    // }
+
+   
 }
+
+
