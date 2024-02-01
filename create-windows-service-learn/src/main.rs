992fdb18704a304903d310f2074b09c0fe6240d4
Tao Tao9 Luo <luotao9@lenovo.com>
2024-02-01 11:12:26 +0800

    test create windows service

--- create-windows-service-learn/src/main.rs
+++ create-windows-service-learn/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate windows_service;
+
+use std::ffi::OsString;
+use windows_service::service_dispatcher;
+
+define_windows_service!(ffi_service_main, my_service_main);
+
+fn my_service_main(arguments: Vec<OsString>) {
+    // The entry point where execution will start on a background thread after a call to
+    // `service_dispatcher::start` from `main`.
+    println!("start my service goooooooooooooo!");
+}
+
+fn main() -> Result<(), windows_service::Error> {
+    // Register generated `ffi_service_main` with the system and start the service, blocking
+    // this thread until the service is stopped.
+    service_dispatcher::start("rs_myservice", ffi_service_main)?;
+    Ok(())
 }
