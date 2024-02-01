66f4987f9db6e3d96de1797a0e632b263ecb756d
Tao Tao9 Luo <luotao9@lenovo.com>
2024-02-01 11:12:57 +0800

    error handle

--- error-learn/src/main.rs
+++ error-learn/src/main.rs
@@ -3,18 +3,30 @@ use std::env;
 use anyhow::Result;
 
 fn main() {
-    println!("Hello, world!:{:?}",env::var("RUST_BACKTRACE"));
+    println!("Hello, world!:{:?}", env::var("RUST_BACKTRACE"));
 
-    env::set_var("RUST_BACKTRACE", "full");
+    env::set_var("RUST_BACKTRACE", "1");
     //env::set_var("RUST_BACKTRACE", "1");
-    println!("error:{:?}",env::var("RUST_BACKTRACE"));
+    println!("error:{:?}", env::var("RUST_BACKTRACE"));
     //panic!("asg");
     if let Err(e) = test_anyhow() {
-        println!("e:{:?} , anyhow backtrace:{:#?}", e, e.backtrace());
+        println!("e:{:?} ", e);
     }
 }
 
 fn test_anyhow() -> Result<i32> {
+    test_anyhow2()
+}
+
+fn test_anyhow2() -> Result<i32> {
+    test_anyhow3()
+}
+
+fn test_anyhow3() -> Result<i32> {
+    test_anyhow4()
+}
+
+fn test_anyhow4() -> Result<i32> {
     return Err(anyhow::anyhow!("error !!!!!!!!!"));
 
     Ok(2)
