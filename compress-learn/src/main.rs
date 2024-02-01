25db67d78d3f26b459931844e2e969620bf339a6
Tao Tao9 Luo <luotao9@lenovo.com>
2024-02-01 11:11:50 +0800

    compress test

--- compress-learn/src/main.rs
+++ compress-learn/src/main.rs
@@ -1,8 +1,12 @@
-use std::{fs, io::prelude::*, path::Path};
+use anyhow::anyhow;
+use log::error;
+use zip::{ZipWriter, write::FileOptions, CompressionMethod};
+use std::{fs::{self, File}, io::prelude::*, path::Path};
 
 fn main() {
-    let mut t = Path::new("HEC");
-    extract(Path::new("HEC-platform.zip"), t).unwrap();
+    let mut t = Path::new("UNZIP");
+    //extract(Path::new("64.zip.001"), t).unwrap();
+    unzip(&Path::new("x.zip"), &t, false).unwrap();
 }
 
 ///解压
@@ -30,6 +34,65 @@ fn extract(test: &Path, target: &Path) -> anyhow::Result<()> {
             std::io::copy(&mut file, &mut target_file)?;
         }
     }
+
+    Ok(())
+}
+
+pub fn unzip(source: &Path, target: &Path, is_ignore_err: bool) -> anyhow::Result<()> {
+    let zipfile = std::fs::File::open(&source).unwrap();
+    let mut zip = zip::ZipArchive::new(zipfile).unwrap();
+    zip.extract(target)?;
+
+    // if !target.exists() {
+    //     fs::create_dir_all(target)?
+    // }
+    // for i in 0..zip.len() {
+    //     let mut file = zip.by_index(i)?;
+    //     if file.is_dir() {
+    //         let target = target.join(Path::new(&file.name().replace("\\", "")));
+    //         fs::create_dir_all(target)?;
+    //     } else {
+    //         let file_path = target.join(Path::new(file.name()));
+    //         if file_path.exists() {
+    //             if let Err(err) = fs::remove_file(&file_path) {
+    //                 if is_ignore_err {
+    //                     error!(
+    //                         "fs::remove_file target_file:{:?} error:{:?}",
+    //                         file_path, err
+    //                     );
+    //                 } else {
+    //                     return Err(anyhow!(
+    //                         "fs::remove_file target_file:{:?} error:{:?}",
+    //                         file_path,
+    //                         err
+    //                     ));
+    //                 }
+    //             }
+    //         }
+    //         let mut target_file = if !file_path.exists() {
+    //             fs::File::create(file_path)?
+    //         } else {
+    //             fs::File::open(file_path)?
+    //         };
+    //         if let Err(err) = std::io::copy(&mut file, &mut target_file) {
+    //             if is_ignore_err {
+    //                 error!(
+    //                     "unzip copy file from:{:?} to target_file:{:?} error:{:?}",
+    //                     file.name(),
+    //                     target_file,
+    //                     err
+    //                 );
+    //             } else {
+    //                 return Err(anyhow!(
+    //                     "unzip copy file from:{:?} to target_file:{:?} error:{:?}",
+    //                     file.name(),
+    //                     target_file,
+    //                     err
+    //                 ));
+    //             }
+    //         }
+    //     }
+    // }
 
     Ok(())
 }
